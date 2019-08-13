489b0ebf0df852d560b934aa1ba2ddcc9d6d4597
gabriela <gabriela.leal@hotmail.com>
2019-08-13 18:27:33 -0300

    Create intro page

---
+++ src/pages/AboutMe.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  v-container
+    v-layout.text-center(column fill-height)
+      h1 Hello, World!
+      div This is my Vue template that includes Vuetify, Vue Router and my beloved Pug.
+      div
+        | You can check for the documentation right 
+        a(src='https://github.com/gabrielaleal/vue-template/blob/master/README.md') here
+        |  if you want to create if from 0 or if you are just curious :)
+</template>
